199b248926c9ce8779d14ff916dd3161802b3e06
naimur1046 <naimurrahman1046@gmail.com>
2025-11-17 18:24:55 +0600

    remove unnecessary part of the code

--- MiniLibrary/MiniLibrary.API/Program.cs
+++ MiniLibrary/MiniLibrary.API/Program.cs
@@ -5,32 +5,23 @@ using Serilog;
 try
 {
     WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-    // Configure Serilog
+    
     builder.Host.UseSerilog((context, loggerConfig) =>
         loggerConfig.ReadFrom.Configuration(context.Configuration));
-
-    // Add services to the container
+    
     builder.Services.AddApiServices(builder.Configuration);
 
     WebApplication app = builder.Build();
-
-    // Configure the HTTP request pipeline
+    
     if (app.Environment.IsDevelopment())
     {
         app.UseSwaggerWithUi();
-
-        // Apply migrations in development
-        // app.ApplyMigrations();
     }
-
-    // Request context logging for correlation
+    
     app.UseRequestContextLogging();
-
-    // Serilog request logging
+    
     app.UseSerilogRequestLogging();
-
-    // Global exception handler
+    
     app.UseExceptionHandler();
 
     app.UseHttpsRedirection();
@@ -40,8 +31,7 @@ try
     app.UseAuthentication();
 
     app.UseAuthorization();
-
-    // Map all endpoints
+    
     app.MapEndpoints();
 
     await app.RunAsync();
@@ -53,10 +43,4 @@ catch (Exception e)
 finally
 {
     await Log.CloseAndFlushAsync();
-}
-
-// REMARK: Required for functional and integration tests to work.
-namespace MiniLibrary.API
-{
-    public partial class Program;
 }
