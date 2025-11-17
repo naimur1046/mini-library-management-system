72163020333b5671840463a719ec479da565ae1a
Naimur Rahman <naimur.onnorkomo@gmail.com>
2025-11-17 12:45:35 +0600

    If any exceptions in the app starting case then catch the exceptions

--- MiniLibrary/MiniLibrary.API/Program.cs
+++ MiniLibrary/MiniLibrary.API/Program.cs
@@ -4,53 +4,64 @@ using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using MiniLibrary.API.Extensions;
 using Serilog;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-// Configure Serilog
-builder.Host.UseSerilog((context, loggerConfig) =>
-    loggerConfig.ReadFrom.Configuration(context.Configuration));
-
-// Add services to the container
-builder.Services.AddApiServices(builder.Configuration);
-
-WebApplication app = builder.Build();
-
-// Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwaggerWithUi();
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-    // Apply migrations in development
-    // app.ApplyMigrations();
+    // Configure Serilog
+    builder.Host.UseSerilog((context, loggerConfig) =>
+        loggerConfig.ReadFrom.Configuration(context.Configuration));
+
+    // Add services to the container
+    builder.Services.AddApiServices(builder.Configuration);
+
+    WebApplication app = builder.Build();
+
+    // Configure the HTTP request pipeline
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwaggerWithUi();
+
+        // Apply migrations in development
+        // app.ApplyMigrations();
+    }
+
+    // Health checks endpoint
+    app.MapHealthChecks("health", new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
+    // Request context logging for correlation
+    app.UseRequestContextLogging();
+
+    // Serilog request logging
+    app.UseSerilogRequestLogging();
+
+    // Global exception handler
+    app.UseExceptionHandler();
+
+    app.UseHttpsRedirection();
+
+    app.UseCors("AllowAll");
+
+    app.UseAuthentication();
+
+    app.UseAuthorization();
+
+    // Map all endpoints
+    app.MapEndpoints();
+
+    await app.RunAsync();
 }
-
-// Health checks endpoint
-app.MapHealthChecks("health", new HealthCheckOptions
+catch (Exception e)
 {
-    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-});
-
-// Request context logging for correlation
-app.UseRequestContextLogging();
-
-// Serilog request logging
-app.UseSerilogRequestLogging();
-
-// Global exception handler
-app.UseExceptionHandler();
-
-app.UseHttpsRedirection();
-
-app.UseCors("AllowAll");
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-// Map all endpoints
-app.MapEndpoints();
-
-await app.RunAsync();
+    Log.Error(e, "An error occurred while starting the Application", e.Message);
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
 
 // REMARK: Required for functional and integration tests to work.
 namespace MiniLibrary.API
